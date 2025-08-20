6f518642f1c740bc2b986c8e216433a702e9f9bf
Phantomvv1 <krisi_andreev@abv.bg>
2025-08-20 11:21:55 +0300

    Made an endpoint for getting historical quotes

--- server/cmd/kay_trade/main.go
+++ server/cmd/kay_trade/main.go
@@ -98,6 +98,7 @@ func main() {
 	data.GET("/bars/latest", marketdata.GetLatestBars)
 	data.GET("/conditions/:ticktype", marketdata.GetConditionCodes)
 	data.GET("/exchanges", marketdata.GetExchangeCodes)
+	data.GET("/quotes", marketdata.GetHisoticalQuotes)
 
 	r.Run(":42069")
 }
