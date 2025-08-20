1a01165a3cd032254f40792854397680c819bcce
Phantomvv1 <krisi_andreev@abv.bg>
2025-08-20 11:27:45 +0300

    Made an endpoint for getting the latest quotes

--- server/cmd/kay_trade/main.go
+++ server/cmd/kay_trade/main.go
@@ -99,6 +99,7 @@ func main() {
 	data.GET("/conditions/:ticktype", marketdata.GetConditionCodes)
 	data.GET("/exchanges", marketdata.GetExchangeCodes)
 	data.GET("/quotes", marketdata.GetHisoticalQuotes)
+	data.GET("/quotes/latest", marketdata.GetLatestQuotes)
 
 	r.Run(":42069")
 }
