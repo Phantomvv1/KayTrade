54d5bb48f1f551c116aca3484424991a584c3480
Phantomvv1 <krisi_andreev@abv.bg>
2026-05-23 17:32:41 +0300

    Made a function that gives you the date when the given stock market was last open

--- server/internal/clock/clock.go
+++ server/internal/clock/clock.go
@@ -1,8 +1,11 @@
 package clock
 
 import (
+	"errors"
+	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	. "github.com/Phantomvv1/KayTrade/internal/exit"
 	. "github.com/Phantomvv1/KayTrade/internal/requests"
@@ -29,21 +32,72 @@ func GetCalendar(c *gin.Context) {
 	headers := BasicAuth()
 	market := c.Param("market")
 	timezone := c.Query("timezone")
+	start := c.Query("start")
+	end := c.Query("end")
 
 	baseUrl := []byte(BaseURL)
 	baseUrl[len(baseUrl)-2] = '2'
 
-	var body any
-	var err error
+	writtenQuestionMark := false
+	urlToReach := string(baseUrl) + Calendar + market
 	if timezone != "" {
-		body, err = SendRequest[any](http.MethodGet, string(baseUrl)+Calendar+market+"?timezone="+timezone, nil, nil, headers)
-	} else {
-		body, err = SendRequest[any](http.MethodGet, string(baseUrl)+Calendar+market, nil, nil, headers)
+		writtenQuestionMark = true
+		urlToReach += "?timezone=" + timezone
 	}
+
+	if start != "" {
+		if !writtenQuestionMark {
+			urlToReach += "?start=" + start
+		}
+
+		urlToReach += "&start=" + start
+	}
+
+	if end != "" {
+		if !writtenQuestionMark {
+			urlToReach += "?end=" + end
+		}
+
+		urlToReach += "&end=" + end
+	}
+
+	body, err := SendRequest[any](http.MethodGet, urlToReach, nil, nil, headers)
 	if err != nil {
 		RequestExit(c, body, err, "coludn't get the clock")
 		return
 	}
 
 	c.JSON(http.StatusOK, body)
+}
+
+func GetLastMarketOpenDay(market string) (*time.Time, error) {
+	headers := BasicAuth()
+
+	baseUrl := []byte(BaseURL)
+	baseUrl[len(baseUrl)-2] = '2'
+
+	start := time.Now().UTC().AddDate(0, 0, -14).Format(time.DateOnly)
+	end := time.Now().UTC().Format(time.DateOnly)
+	body, err := SendRequest[map[string]any](http.MethodGet, string(baseUrl)+Calendar+market+"?timezone=UTC&start="+start+"&end="+end, nil, nil, headers)
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now().UTC()
+	calendarInfo := body["calendar"].([]any)
+	for i := range calendarInfo {
+		info := calendarInfo[len(calendarInfo)-(i+1)].(map[string]any)
+		coreStart := info["core_start"].(string)
+
+		startTs, err := time.Parse(time.RFC3339, coreStart)
+		if err != nil {
+			return nil, err
+		}
+
+		if now.After(startTs) {
+			return &startTs, nil
+		}
+	}
+
+	return nil, errors.New("Error: wasn't able to find the last day the given stock market was open")
 }
